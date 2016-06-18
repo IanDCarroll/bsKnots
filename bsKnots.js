ad993f1a1c8654c360aa817a520a60d2729c733b
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-18 07:51:29 -0700

    add JS rvrs bsKnot

--- bsKnots.js
+++ bsKnots.js
@@ -1,1 +1,9 @@
-console.log('Boogers');
+function rvrs(string) {
+    var rvrs = ''
+    for ( i=string.length-1; i>=0; i-- ) {
+	rvrs += string[i];
+    }
+    return rvrs;
+}
+
+console.log(rvrs('boogers'));
