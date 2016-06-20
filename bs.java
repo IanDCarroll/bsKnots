31e4e3424f1b9a1ca1c81b00c6b465d0ff41568d
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-20 07:32:21 -0700

    port rPal() to JS; have Java not cooperate

--- bs.java
+++ bs.java
@@ -6,10 +6,22 @@ class Knots {
 	}
 	return rvrs;
     }
+
+    public static String rPal(String text) {
+	String message = "Hey... " + text + " isn\'t a palendrome. How am I supposed to reverse that?!";
+	//If statement always returns else...
+	if (text == rvrs(text)) {
+	    return text;
+	} else {
+	    return message;
+	}
+    }
 }
 
 public class bs {
     public static void main(String [] args) {
 	System.out.println(Knots.rvrs("majestic unicorn"));
+	System.out.println(Knots.rPal("racecar"));
+	System.out.println(Knots.rPal("lacecar"));
     }
 }
--- bsKnots.js
+++ bsKnots.js
@@ -1,4 +1,4 @@
-function rvrs(string) {
+function rvrsKnot(string) {
     var rvrs = ''
     for ( i=string.length-1; i>=0; i-- ) {
 	rvrs += string[i];
@@ -6,4 +6,16 @@ function rvrs(string) {
     return rvrs;
 }
 
-console.log(rvrs('boogers'));
+function rPalKnot(string) {
+    var message = 'Hey... ' + string + 
+	' isn\'t a palendrome. How am I supposed to reverse that?!';
+    if (string === rvrsKnot(string)) {
+	return string;
+    } else {
+	return message;
+    }
+}
+
+console.log(rvrsKnot('boogers'));
+console.log(rPalKnot('racecar'));
+console.log(rPalKnot('lacecar'));
