f9675a475ffe63ac278aef1aaf12d2b85ba1fd81
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-21 07:39:35 -0700

    make a JS vowel movement function

--- bsKnots.js
+++ bsKnots.js
@@ -16,6 +16,22 @@ function rPalKnot(string) {
     }
 }
 
+function vmmtKnot(string) {
+    var vowelMovement = '',
+	test = /[aeiouy]/,
+	vowel = [ 'a', 'e', 'i', 'o', 'u', 'y' ];
+    for ( i=0; i<string.length; i++ ) {
+	if (string[i].search(test) !== -1) {
+	    //replace it with a random vowel
+	    vowelMovement += vowel[Math.floor(Math.random()*vowel.length)];
+	} else {
+	    vowelMovement += string[i];
+	}
+    }
+    return vowelMovement;
+}
+
 console.log(rvrsKnot('boogers'));
 console.log(rPalKnot('racecar'));
 console.log(rPalKnot('lacecar'));
+console.log(vmmtKnot('I\'m having a vowel movement!'));
